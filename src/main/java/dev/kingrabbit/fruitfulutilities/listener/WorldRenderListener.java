e9a94ed1fe72702f7e9924a5c66095833416402e
KingsDev <adking08@hotmail.com>
2023-07-04 15:39:26 +1000

    Implemented Hide If Unlocked into the beacons

--- src/main/java/dev/kingrabbit/fruitfulutilities/listener/WorldRenderListener.java
+++ src/main/java/dev/kingrabbit/fruitfulutilities/listener/WorldRenderListener.java
@@ -3,6 +3,7 @@ package dev.kingrabbit.fruitfulutilities.listener;
 import com.google.gson.JsonObject;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.kingrabbit.fruitfulutilities.FruitfulUtilities;
+import dev.kingrabbit.fruitfulutilities.config.categories.PathViewerCategory;
 import dev.kingrabbit.fruitfulutilities.pathviewer.PathManager;
 import me.x150.renderer.event.RenderEvents;
 import me.x150.renderer.render.Renderer3d;
@@ -23,8 +24,10 @@ public class WorldRenderListener implements RenderEvents.RenderEvent, WorldRende
 
     @Override
     public void rendered(MatrixStack matrices) {
+        PathViewerCategory category = FruitfulUtilities.getInstance().configManager.getCategory(PathViewerCategory.class);
+
         for (JsonObject upgrade : PathManager.tracking) {
-            if (PathManager.purchased.contains(upgrade.get("display").getAsString())) continue;
+            if (PathManager.purchased.contains(upgrade.get("display").getAsString()) && category.hideIfUnlocked) continue;
 
             String[] location = upgrade.get("location").getAsString().split(",");
             if (location.length != 3)
@@ -49,9 +52,11 @@ public class WorldRenderListener implements RenderEvents.RenderEvent, WorldRende
     public void onEnd(WorldRenderContext context) {
         if (!FruitfulUtilities.getInstance().configManager.enabled()) return;
 
+        PathViewerCategory category = FruitfulUtilities.getInstance().configManager.getCategory(PathViewerCategory.class);
+
         Vec3d playerPos = MinecraftClient.getInstance().player.getPos();
         for (JsonObject upgrade : PathManager.tracking) {
-            if (PathManager.purchased.contains(upgrade.get("display").getAsString())) continue;
+            if (PathManager.purchased.contains(upgrade.get("display").getAsString()) && category.hideIfUnlocked) continue;
 
             String[] location = upgrade.get("location").getAsString().split(",");
             if (location.length != 3) {
@@ -61,11 +66,9 @@ public class WorldRenderListener implements RenderEvents.RenderEvent, WorldRende
             int y = Integer.parseInt(location[1]);
             int z = Integer.parseInt(location[2]);
 
-            Renderer3d.renderThroughWalls();
             Vec3d upgradePos = new Vec3d(x, y, z);
             double distance = playerPos.distanceTo(upgradePos);
             int alpha = (int) Math.min(Math.max(Math.pow(2, 0.5 * distance) * 5, 50), 255);
-            Renderer3d.stopRenderThroughWalls();
             if (distance >= 5) {
 //                System.out.println(x);
                 context.matrixStack().push();
