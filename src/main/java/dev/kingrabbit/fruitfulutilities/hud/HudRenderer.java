6a5234e15919389ca7cacf51c2304096cf6af025
KingsDev <adking08@hotmail.com>
2023-07-04 16:54:38 +1000

    Added <opac> tag for HUD elements

--- src/main/java/dev/kingrabbit/fruitfulutilities/hud/HudRenderer.java
+++ src/main/java/dev/kingrabbit/fruitfulutilities/hud/HudRenderer.java
@@ -7,6 +7,7 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.OrderedText;
+import net.minecraft.text.Text;
 
 import java.awt.*;
 import java.util.LinkedHashMap;
@@ -30,16 +31,36 @@ public class HudRenderer implements HudRenderCallback {
                 int y = (int) elementClass.getField("y").get(element);
 
                 TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
+                int totalLines = lines.size();
                 int maxWidth = 0;
                 for (Object line : lines) {
-                    int lineWidth = line instanceof String ? textRenderer.getWidth((String) line) : textRenderer.getWidth((OrderedText) line);
+                    if (line instanceof String lineString) {
+                        if (lineString.startsWith("<opac>")) {
+                            totalLines--;
+                            continue;
+                        }
+                    }
+                    int lineWidth = line instanceof String ? textRenderer.getWidth((String) line) : (line instanceof OrderedText ? textRenderer.getWidth((OrderedText) line) : textRenderer.getWidth((Text) line));
                     if (lineWidth > maxWidth) maxWidth = lineWidth;
                 }
-                DrawableHelper.fill(matrices, x, y, x + 4 + maxWidth, y + 2 + (textRenderer.fontHeight + 2) * lines.size(), BACKGROUND_COLOUR.getRGB());
+                DrawableHelper.fill(matrices, x, y, x + 4 + maxWidth, y + 2 + (textRenderer.fontHeight + 2) * totalLines, BACKGROUND_COLOUR.getRGB());
+                int opacity = 100;
                 for (Object line : lines) {
-                    if (line instanceof String) DrawableHelper.drawTextWithShadow(matrices, textRenderer, (String) line, x + 2, y + 2, 0xFFFFFF);
-                    else if (line instanceof OrderedText) DrawableHelper.drawTextWithShadow(matrices, textRenderer, (OrderedText) line, x + 2, y + 2, 0xFFFFFF);
+//                    System.out.println(opacity);
+                    if (line instanceof String stringLine) {
+                        if (stringLine.startsWith("<opac>")) {
+                            try {
+                                opacity = Integer.parseInt(stringLine.substring(6, stringLine.length() - 7));
+                            } catch (NumberFormatException exception) {
+                                exception.printStackTrace();
+                            }
+                            continue;
+                        }
+                        DrawableHelper.drawTextWithShadow(matrices, textRenderer, stringLine, x + 2, y + 2, new Color(255, 255, 255, (int) (opacity * 2.55)).getRGB());
+                    } else if (line instanceof OrderedText) DrawableHelper.drawTextWithShadow(matrices, textRenderer, (OrderedText) line, x + 2, y + 2, new Color(255, 255, 255, (int) (opacity * 2.55)).getRGB());
+                    else if (line instanceof Text) DrawableHelper.drawTextWithShadow(matrices, textRenderer, (Text) line, x + 2, y + 2, new Color(255, 255, 255, (int) (opacity * 2.55)).getRGB());
                     y += 2 + textRenderer.fontHeight;
+                    opacity = 100;
                 }
             } catch (IllegalAccessException | NoSuchFieldException e) {
                 e.printStackTrace();
