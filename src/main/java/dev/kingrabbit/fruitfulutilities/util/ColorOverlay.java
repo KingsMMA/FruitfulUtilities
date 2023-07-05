a26f43bd3df5e15581a491b720000a333742adff
KingsDev <adking08@hotmail.com>
2023-07-05 11:20:40 +1000

    Simplified ColorOverlay

--- src/main/java/dev/kingrabbit/fruitfulutilities/util/ColorOverlay.java
+++ src/main/java/dev/kingrabbit/fruitfulutilities/util/ColorOverlay.java
@@ -4,23 +4,11 @@ import java.awt.Color;
 
 public class ColorOverlay {
     public static Color overlayColors(Color colorA, Color colorB, double opacity) {
-        // Extract the RGB components of colorA
-        int redA = colorA.getRed();
-        int greenA = colorA.getGreen();
-        int blueA = colorA.getBlue();
-
-        // Extract the RGB components of colorB
-        int redB = colorB.getRed();
-        int greenB = colorB.getGreen();
-        int blueB = colorB.getBlue();
-
-        // Calculate the resulting RGB components
-        int redResult = (int) (redA * (1 - opacity) + redB * opacity);
-        int greenResult = (int) (greenA * (1 - opacity) + greenB * opacity);
-        int blueResult = (int) (blueA * (1 - opacity) + blueB * opacity);
-
-        // Create and return the resulting color
-        return new Color(redResult, greenResult, blueResult);
+        return new Color(
+                (int) (colorA.getRed() * (1 - opacity) + colorB.getRed() * opacity),
+                (int) (colorA.getGreen() * (1 - opacity) + colorB.getGreen() * opacity),
+                (int) (colorA.getBlue() * (1 - opacity) + colorB.getBlue() * opacity)
+        );
     }
 
 }
