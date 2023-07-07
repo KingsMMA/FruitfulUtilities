303c0ba5db377886bf5d45a0df9a4d53a398f970
KingsMMA <adking08@hotmail.com>
2023-07-07 23:11:23 +1000

    Changed purchased upgrades to ID
    Significantly better than by display name

--- src/main/java/dev/kingrabbit/fruitfulutilities/pathviewer/PathManager.java
+++ src/main/java/dev/kingrabbit/fruitfulutilities/pathviewer/PathManager.java
@@ -18,8 +18,9 @@ import java.util.List;
 public class PathManager {
 
     public static final HashMap<String, String> pathParents = new HashMap<>();
-    public static final ArrayList<String> purchased = new ArrayList<>();
+    public static final ArrayList<String> purchasedIds = new ArrayList<>();
     public static final HashMap<String, JsonObject> paths = new HashMap<>();
+    public static final HashMap<JsonObject, String> upgradeToId = new HashMap<>();
     public static final HashMap<JsonObject, String> upgradeToPath = new HashMap<>();
     public static final ArrayList<JsonObject> tracking = new ArrayList<>();
 
@@ -43,6 +44,7 @@ public class PathManager {
             JsonObject path = paths.get(pathId);
             for (String upgradeId : path.keySet()) {
                 JsonObject upgrade = path.getAsJsonObject(upgradeId);
+                upgradeToId.put(upgrade, upgradeId);
                 upgradeToPath.put(upgrade, pathId);
             }
         }
@@ -58,20 +60,28 @@ public class PathManager {
         }
     }
 
+    public static String getId(JsonObject upgrade) {
+        if (upgradeToId.containsKey(upgrade)) return upgradeToId.get(upgrade);
+        FruitfulUtilities.LOGGER.warn("Couldn't get ID for upgrade with name \"" + upgrade.get("display").getAsString() + "\".");
+        return null;
+    }
+
     public static void unlocked(String upgradeName) {
-        purchased.add(upgradeName);
         for (JsonObject path : paths.values()) {
             for (String upgradeId : path.keySet()) {
                 JsonObject upgrade = path.getAsJsonObject(upgradeId);
                 if (upgrade.get("display").getAsString().equals(upgradeName)) {
+                    purchasedIds.add(getId(upgrade));
                     if (upgrade.has("path")) {
                         String newPath = upgrade.get("path").getAsString();
                         if (!PathScreen.sections.containsKey(newPath))
                             PathScreen.sections.put(newPath, new float[]{-19284, -64, 1});
                     }
+                    return;
                 }
             }
         }
+        FruitfulUtilities.LOGGER.warn("Unable to find upgrade with name \"" + upgradeName + "\".");
     }
 
     public static List<JsonObject> requiredToUnlock(JsonObject upgrade) {
@@ -81,7 +91,8 @@ public class PathManager {
     public static List<JsonObject> requiredToUnlock(JsonObject upgrade, boolean includeUpgrade, boolean disableIfNotCumulative) {
         List<JsonObject> allRequired = new ArrayList<>();
 
-        if (purchased.contains(upgrade.get("display").getAsString())) return allRequired;
+        String upgradeId = getId(upgrade);
+        if (purchasedIds.contains(upgradeId)) return allRequired;
 
         if (includeUpgrade) allRequired.add(upgrade);
 
@@ -97,7 +108,7 @@ public class PathManager {
                 String requiredId = _requiredId.getAsString();
                 JsonObject required = findUpgrade(path, requiredId);
                 if (required != null) {
-                    if (purchased.contains(required.get("display").getAsString())) continue;
+                    if (purchasedIds.contains(getId(required))) continue;
                     List<JsonObject> requiredPath = requiredToUnlock(required);
                     for (JsonObject req : requiredPath)
                         if (!allRequired.contains(req))
@@ -142,7 +153,7 @@ public class PathManager {
     }
 
     public static boolean locked(JsonObject upgrade) {
-        if (purchased.contains(upgrade.get("display").getAsString())) return false;
+        if (purchasedIds.contains(getId(upgrade))) return false;
 
         if (upgrade.has("path")) {
             String path = upgrade.get("path").getAsString();
@@ -163,7 +174,7 @@ public class PathManager {
             if (not) requiredUpgradeId = requiredUpgradeId.substring(1);
             JsonObject requiredUpgrade = findUpgrade(path, requiredUpgradeId);
             if (requiredUpgrade == null) continue;
-            boolean unlocked = purchased.contains(requiredUpgrade.get("display").getAsString());
+            boolean unlocked = purchasedIds.contains(getId(requiredUpgrade));
             if ((not && unlocked) || (!not && !unlocked)) return true;
         }
         return false;
