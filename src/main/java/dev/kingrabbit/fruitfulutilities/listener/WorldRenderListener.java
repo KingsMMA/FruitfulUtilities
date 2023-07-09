713223a5c7668e8e401cf55320db2173a3710d0c
KingsMMA <adking08@hotmail.com>
2023-07-09 19:55:25 +1000

    Fixed rendering, readded block highlight

--- src/main/java/dev/kingrabbit/fruitfulutilities/listener/WorldRenderListener.java
+++ src/main/java/dev/kingrabbit/fruitfulutilities/listener/WorldRenderListener.java
@@ -7,11 +7,12 @@ import dev.kingrabbit.fruitfulutilities.pathviewer.PathManager;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Vec3d;
+import org.joml.Matrix4f;
 
-import java.awt.*;
 import java.util.Objects;
 import java.util.logging.Logger;
 
@@ -28,24 +29,80 @@ public class WorldRenderListener implements WorldRenderEvents.End {
             String[] location = upgrade.get("location").getAsString().split(",");
             if (location.length != 3)
                 Logger.getGlobal().severe("Unable to parse location of " + upgrade.get("display").getAsString() + " (" + upgrade.get("location").getAsString() + ")");
-            int x1 = Integer.parseInt(location[0]);
-            int y1 = Integer.parseInt(location[1]);
-            int z1 = Integer.parseInt(location[2]);
-            int x2 = x1 + 1;
-            int y2 = y1 + 1;
-            int z2 = z1 + 1;
 
-            Vec3d upgradePos = new Vec3d(x, y, z);
-            double distance = playerPos.distanceTo(upgradePos);
-            int alpha = (int) Math.min(Math.max(Math.pow(2, 0.5 * distance) * 5, 50), 255);
+            int rawX = Integer.parseInt(location[0]);
+            int rawY = Integer.parseInt(location[1]);
+            int rawZ = Integer.parseInt(location[2]);
 
+            Vec3d upgradePos = new Vec3d(rawX, rawY, rawZ);
+            MatrixStack matrices = context.matrixStack();
+            Matrix4f matrix = matrices.peek().getPositionMatrix();
             Vec3d offset = upgradePos.subtract(MinecraftClient.getInstance().gameRenderer.getCamera().getPos());
 
-            MatrixStack matrices = context.matrixStack();
+            double distance = playerPos.distanceTo(upgradePos);
+            float red = 140 / 255f;
+            float green = 0f;
+            float blue = 250 / 255f;
+            float alpha = ((int) Math.min(Math.max(Math.pow(2, 0.5 * distance) * 5, 50), 255)) / 255f;
+
+            if (true) {
+                float x1, x2, y1, y2, z1, z2;
+                x1 = x2 = (float) offset.x;
+                y1 = y2 = (float) offset.y;
+                z1 = z2 = (float) offset.z;
+                x2++;
+                y2++;
+                z2++;
+
+                RenderSystem.enableBlend();
+                RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+                RenderSystem.disableDepthTest();
+                RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+
+                Tessellator tessellator = Tessellator.getInstance();
+                BufferBuilder buffer = tessellator.getBuffer();
+
+                buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+                buffer.vertex(matrix, x1, y2, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y2, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y2, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y2, z1).color(red, green, blue, alpha).next();
+
+                buffer.vertex(matrix, x1, y1, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y1, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y2, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y2, z2).color(red, green, blue, alpha).next();
+
+                buffer.vertex(matrix, x2, y2, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y1, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y1, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y2, z1).color(red, green, blue, alpha).next();
+
+                buffer.vertex(matrix, x2, y2, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y1, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y1, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y2, z1).color(red, green, blue, alpha).next();
+
+                buffer.vertex(matrix, x1, y2, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y1, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y1, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y2, z2).color(red, green, blue, alpha).next();
+
+                buffer.vertex(matrix, x1, y1, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y1, z1).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x2, y1, z2).color(red, green, blue, alpha).next();
+                buffer.vertex(matrix, x1, y1, z2).color(red, green, blue, alpha).next();
+
+                BufferRenderer.drawWithGlobalProgram(buffer.end());
+
+                RenderSystem.enableCull();
+                RenderSystem.disableBlend();
+                RenderSystem.enableDepthTest();
+            }
+
             if (distance >= 5) {
                 matrices.push();
-                matrices.translate(offset.x, offset.y, offset.z);
-                RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha / 255f);
+                matrices.translate(offset.x, offset.y + 1, offset.z);
                 BeaconBlockEntityRenderer.renderBeam(
                         matrices,
                         Objects.requireNonNull(context.consumers()),
@@ -55,7 +112,7 @@ public class WorldRenderListener implements WorldRenderEvents.End {
                         context.world().getTime(),
                         0,
                         255,
-                        new float[]{140 / 255f, 0 / 255f, 250 / 255f},
+                        new float[]{red, green, blue},
                         0.2f,
                         0f
                 );
