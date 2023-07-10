4be258e15623f87f8c046ad3528621fa2267daaa
KingsMMA <adking08@hotmail.com>
2023-07-10 16:47:18 +1000

    Setup Mixin

---
+++ src/main/java/dev/kingrabbit/fruitfulutilities/mixin/InGameHudMixin.java
@@ -0,0 +1,8 @@
+package dev.kingrabbit.fruitfulutilities.mixin;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+}
